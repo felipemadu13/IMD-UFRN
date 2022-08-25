bc8c2d7fe6431e2a8031244bdf3357cfcfa5d645
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-08-25 10:10:55 -0300

    teste

---
+++ Atividades_Moodle/Atividade_03-1/Atividade_03_1.js
@@ -0,0 +1,8 @@
+        function soma_e_escreve() {
+            var x = Number(document.getElementById("entrada1").value);
+            var y = Number(document.getElementById("entrada2").value);
+  
+            var z = x + y;
+
+            document.getElementById("saida").innerHTML = z;
+        }
