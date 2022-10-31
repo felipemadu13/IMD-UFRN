fd737636d134d5bad252e60f6a517323d1920375
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-10-31 11:32:24 -0300

    Aula 02

---
+++ Programação Orientada a Objetos/Atividades_Moodle/Aula 02/Atividade 01/atividade_01.js
@@ -0,0 +1,11 @@
+var Moto = /** @class */ (function () {
+    function Moto() {
+    }
+    return Moto;
+}());
+var c = new Moto();
+c.modelo = "XPT-0";
+c.cor = "Preto";
+c.placa = "NNN-9999";
+c.cilindradas = 100;
+console.log(c);
---
+++ Programação Orientada a Objetos/Atividades_Moodle/Aula 02/Atividade 01/atividade_01.ts
@@ -0,0 +1,14 @@
+class Moto {
+    modelo: string;
+    cor: string;
+    placa: string;
+    cilindradas: number;
+
+}
+
+let c = new Moto();
+c.modelo = "XPT-0";
+c.cor = "Preto";
+c.placa = "NNN-9999"
+c.cilindradas = 100
+console.log(c);
